6a9480353596799ca3258c6ca50e7baad3fa63e3
github username <github email address>
2018-04-06 14:44:50 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,21 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find_by(params[:id])
+  end
+
 end
+
+## Objectives
+#
+# * Draw a route with a dynamic variable
+#
+# * Use a routes variable in params within the logic of an action
+#
+# * Assign an instance variable in a controller action
+#
+# * Use a controller instance variable to generate a dynamic ERB template.
+#
+# * Build a classical show action/view
